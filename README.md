90bcbc3233fa874b19e2fec27622b1a017b6dee7
Aliaksandr Parfiankou <aliaksandr.parfiankou@gmail.com>
2019-02-21 16:52:33 +0300

    chore: update readme

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # mediacube_employers_web
 Web side of test task of employers grid system
+
+# Features
+### Node.js SSR Rendeing
+### Automatic quick deploy
+### Lightweight API [https://github.com/aliaksandrparfiankou/mediacube_employers_api](https://github.com/aliaksandrparfiankou/mediacube_employers_api)
+
+# [DEMO](https://jjj.by)
+
+# What I've done
+- [x] Company page(grid)
+- [x] Employees page
+- [x] Departments page
+- [x] Page pagination
+- [x] Add employee form
+- [x] Add department form
+- [x] Remove employee
+- [x] Remove department
+- [ ] Edit employee form
+- [ ] Edit department form
+
+# What I didn't do(aka Diseases)
+- No tests, because I didn't have time
+- No cache, see above
+- It's not DRY, see above
+- It's not KISS, see above
+- Creepy validation, see above
+
+# Implementation time
+6-7-ish hours
+
+# My open-source projects
+### [Fistache](https://github.com/fistache/fistache)
+##### front-end framework
+A lot of packages inside including webpack loader(support HMR), 
+component rendered(support SSR, data binding, reactivity(PROXIES) and etc),
+cli (only local nowadays).
+### [Phpstorm, webstorm plugin](https://github.com/fistache/fistache_plugin_idea)
+##### fistache support
+### [Trusted ssl localhost certificate](https://github.com/fistache/express-https-provider)
+##### express.js plugin(well, almost)
---
+++ src/pages/DepartmentEdit.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="page">
+        <div class="page__body">
+            <v-container grid-list-xl>
+                <v-form ref="form">
+                    <v-text-field
+                            :autofocus="true"
+                            :validate-on-blur="true"
+                            :aria-required="true"
+                            v-model="name"
+                            :counter="max"
+                            :rules="nameRules"
+                            required
+                            label="Department name"
+                    ></v-text-field>
+                    <v-btn @click="submit" flat color="green">
+                        <v-icon>add</v-icon>
+                        <span>Create</span>
+                    </v-btn>
+                </v-form>
+            </v-container>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { addDepartment } from '../api'
+
+    export default {
+        asyncData({ store, route }) {
+            return store.dispatch('fetchDepartment', route.params.id)
+        },
+        data() {
+            return {
+                name: '',
+                nameRules: [
+                    v => !!v || 'Name is required',
+                    v => (v && v.length <= 64) || 'Name must be less than 64 characters'
+                ],
+                max: 64,
+            }
+        },
+        computed: {
+            department() {
+            }
+        },
+        methods: {
+            submit () {
+                addDepartment({
+                    name: this.name,
+                }).then(() => {
+                    this.$notify({
+                        type: 'success',
+                        title: 'Done',
+                        text: 'Department had been successfully added.'
+                    })
+                    this.$router.push({
+                        name: 'departments'
+                    })
+                }).catch(error => {
+                    if (error.response.status === 422) {
+                        this.$notify({
+                            type: 'error',
+                            title: 'Error',
+                            text: 'Validation error. \n' + JSON.stringify(error.response.data)
+                        })
+                    } else {
+                        this.$notify({
+                            type: 'error',
+                            title: 'Error',
+                            text: 'Something went wrong.'
+                        })
+                    }
+                })
+            },
+        }
+    }
+</script>
